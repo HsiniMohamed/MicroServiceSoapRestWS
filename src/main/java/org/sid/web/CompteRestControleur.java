7a73ce835cf9b1d3aa24679eca027b9968fad04a
Mohamed Hsini <hssinimohamed5@gmail.com>
2024-02-16 01:41:36 +0100

    Créartion un connecteur REST "Creation Web Service Compte avec RestControlller"

---
+++ src/main/java/org/sid/web/CompteRestControleur.java
@@ -0,0 +1,49 @@
+package org.sid.web;
+
+import java.util.List;
+
+import org.sid.entities.Compte;
+import org.sid.repositories.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/banque")
+public class CompteRestControleur {
+	@Autowired
+	private CompteRepository compteRepository;
+	@GetMapping(path="/comptes",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+	public List<Compte> compteList(){
+			return compteRepository.findAll();
+	}
+	
+	@GetMapping("comptes/{id}")
+	public Compte getOne(@PathVariable Long  id){
+			return compteRepository.findById(id).get();
+	}
+	
+	@PostMapping("/comptes")
+	public Compte save(@RequestBody Compte compte){
+			return compteRepository.save(compte);
+	}
+	
+	@PutMapping("/comptes/{id}")
+	public Compte update(Compte compte, @PathVariable Long id){
+			compte.setId(id);
+			return compteRepository.save(compte);
+	}
+	@DeleteMapping("/comptes/{id}")
+	public void delete( @PathVariable Long id){
+			compteRepository.deleteById(id);
+	}
+
+}
